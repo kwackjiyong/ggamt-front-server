7be97ae8d64111f23f488ef89df25833002ec021
곽지용 <48040031+kwackjiyong@users.noreply.github.com>
2024-09-05 23:28:44 +0000

    feat: css modify

--- src/app/baram/page.tsx
+++ src/app/baram/page.tsx
@@ -131,6 +131,7 @@ export default function BaramChart () {
               <button onClick={handleToggleShowLatest} className="toggle-button">
                 {isShowLatest ? '최근 12시간' : '전체보기'}
               </button>
+              <p> * 연서버 기준으로 조회됩니다.</p>
               <p> * 랭킹 상위 10만명을 기준으로 수집한 데이터입니다.</p>
               <p> * 데이터는 매시간마다 수집됩니다.</p>
               <p> * 수집 데이터 내 오류 발견 시 수집 데이터는 언제든 수정/삭제될 수 있습니다.</p>
