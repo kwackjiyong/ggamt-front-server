c26ec99c9671ff6bebbcf2d8cab9cc25adca4aaf
곽지용 <48040031+kwackjiyong@users.noreply.github.com>
2024-09-12 02:04:43 +0000

    feat: 매크로 현황 표현 수정

--- src/app/baram/macro/page.tsx
+++ src/app/baram/macro/page.tsx
@@ -7,6 +7,7 @@ import { BaramMacroService } from '@/service/baram';
 export default function BaramMacroList () {
     const [macroInfos, setMacroInfos] = useState<Array<MacroLiveInfoInterface>>([]);
     const [macroCnt, setMacroCnt] = useState(0);
+    const [macroLiveCnt, setMacroLiveCnt] = useState(0);
     const [gthrDttm, setGthrDttm] = useState('');
     const baramMacroService = new BaramMacroService();
     useEffect(() => {
@@ -17,6 +18,7 @@ export default function BaramMacroList () {
                 setGthrDttm(res.gthrDttm ?? '');
                 setMacroInfos(res.macroInfos?.toSorted((a, b) => a.isLive ? -1 : 1) ?? []);
                 setMacroCnt(res.macroInfos?.length ?? 0);
+                setMacroLiveCnt(res.macroInfos?.filter(r=>r.isLive)?.length ?? 0);
             }
         }
         requestMacroInfos();
@@ -30,7 +32,7 @@ export default function BaramMacroList () {
         <div>
             <h4 className="baram-title">바람의나라 매크로 조회</h4>
             <div className='baram-desc-box'>
-                <p> * 총 매크로 유저: {macroCnt}명</p>
+                <p> * 현재 매크로 유저 {macroLiveCnt} 명이 접속 중 입니다. (총원 {macroCnt}명 중)</p>
                 <p> * 최근수집일시: {gthrDttm}</p>
                 <p> * 접속여부는 10분마다 갱신됩니다.</p>
                 <p> * 매크로는 쓰지맙시다.</p>
@@ -42,7 +44,7 @@ export default function BaramMacroList () {
                         <img className="macro-character-image" src={getUrl(macro.userName)} alt={macro.userName} />
                         <div className="macro-character-name">{macro.userName}@연</div>
                         <div className="macro-character-desc">{macro.description}</div>
-                        <div className={`macro-character-live ${macro.isLive ? 'on' : ''}` }>{macro.isLive ? '접속중' : '미접속'}</div>
+                        <div className={`macro-character-live ${macro.isLive ? 'on' : ''}` }>{macro.isLive ? 'ON' : 'OFF'}</div>
                     </div>
                 ))}
                 
