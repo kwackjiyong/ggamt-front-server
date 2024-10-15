9781b4c95e58f762d46cda378406bf7b169640eb
곽지용 <48040031+kwackjiyong@users.noreply.github.com>
2024-10-15 07:45:12 +0000

    feat: 서비스 종료 안내문

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
-import Link from 'next/link';
 import * as gtag from '@/util/gtag';
 
 export default function Home() {
   gtag.pageview('/');
   return (
     <div>
-      <Link href="/baram">
-        <div className="btn-round imageButton">
-          바람의나라 동시접속자수 확인하러 가기
-        </div>
-      </Link>
-      <br/>
+      <div className='.baram-desc-box'>
+        다음과 같은 이유로 GGAMT 서비스는 종료 예정입니다.
+
+        1. CAPTCHA 적용으로 인한 유저목록 스크래핑 불가
+        2. 매크로 현황 서비스 삭제 요구 공문 수신
+        3. NexonOpenAPI-바람의나라 응답 파라미터 중 로그인 정보 삭제
+
+        더이상 활용가능한 정보가 없어서 서비스 개발이 어렵게 되었습니다.
+
+        감사합니다.
+      </div>
     </div>
   );
 }
